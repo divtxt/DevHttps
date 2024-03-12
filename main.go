d5060b8b352f928de2a9e921956d6f95b1fb7ef7
Div Shekhar <div@txtlabs.com>
2024-03-12 15:48:40 +0530

    Skeleton cli using urfave/cli

--- main.go
+++ main.go
@@ -1,16 +1,33 @@
 package main
 
 import (
-	"flag"
 	"log"
+	"os"
+
+	"github.com/urfave/cli/v2"
 )
 
 func main() {
-	flag.Parse()
+	app := &cli.App{
+		Commands: []*cli.Command{
+			{
+				Name:  "add",
+				Usage: "Add a development domain and matching development port",
+				Action: func(cCtx *cli.Context) error {
+					return cli.Exit("Not Implemented", 1)
+				},
+			},
+			{
+				Name:  "status",
+				Usage: "Show server configuration and current status",
+				Action: func(cCtx *cli.Context) error {
+					return cli.Exit("Not Implemented", 1)
+				},
+			},
+		},
+	}
 
-	args := flag.Args()
-	if len(args) == 0 {
-		// TODO: show help instead
-		log.Fatal("Please specify a subcommand.")
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
 	}
 }
